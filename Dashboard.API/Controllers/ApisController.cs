9aa4552384290c1381e359ee7ffbbf29f89e0224
Dmytro Potapchuk <traiget@gmail.com>
2024-12-22 12:41:43 +0200

    add test controller

--- Dashboard.API/Controllers/ApisController.cs
+++ Dashboard.API/Controllers/ApisController.cs
@@ -35,5 +35,11 @@ namespace Dashboard.API.Controllers
 
             return BadRequest("error");
         }
+
+        [HttpGet("test")]
+        public IActionResult Test()
+        {
+            return Ok("Test method");
+        }
     }
 }
